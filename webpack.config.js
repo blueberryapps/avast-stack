3c5e5927c2f63cf91cd6365b74d0e89e632f149b
Ondrej Bartas <ondrej@bartas.cz>
2017-03-14 16:31:34 +0100

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const config = {
+  context: path.join(__dirname, '/src'), // `__dirname` is root of project and `src` is source
+  entry: {
+    app: './index.js',
+  },
+  output: {
+    path: path.join(__dirname, '/dist'), // `dist` is the destination
+    filename: '[name].bundle.js',
+  },
+  devServer: {
+    open: true, // to open the local server in browser
+    contentBase: path.join(__dirname, '/src'),
+    noInfo: true,
+    hot: true
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'index.html' })]
+};
+
+module.exports = config;
