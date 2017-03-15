1b8094d7e36be9c55aedb8252d37b7d8275d2cd1
Ondrej Bartas <ondrej@bartas.cz>
2017-03-15 12:50:24 +0100

    Add redux thunk middleware for DI

--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
 import { reducer as onionForm } from 'onion-form';
+import reduxThunk from 'redux-thunk';
 
 const initialState = {};
 const reducers = { onionForm };
-const middlewares = applyMiddleware();
+const middlewares = applyMiddleware(reduxThunk);
 
 const allMiddlewares = window.devToolsExtension
   ? compose(middlewares, window.devToolsExtension())
